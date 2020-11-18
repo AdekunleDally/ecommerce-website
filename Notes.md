e8d0edc3452b18cc8828d53b6fe71cb4c8658e79
AdekunleDally <adekunledally@gmail.com>
2020-11-17 19:48:30 -0800

    header component added to the ecommerce website

--- Notes.md
+++ Notes.md
@@ -1,1 +1,9 @@
 ### Only the <HomePage /> component has access to the history props that we need
+ 
+ ## Importing Logo into a component
+### 1. import {Link} from 'react-router-dom';
+### 2. import {ReactComponent as Logo} from '../../assets/crown.svg';
+
+### 3.  <Link className="logo-container" to="/">
+###       <Logo  className="logo"/>
+###     </Link>
--- src/App.js
+++ src/App.js
@@ -2,18 +2,20 @@ import './App.css';
 import {Route, Switch} from 'react-router-dom';
 import HomePage from "./pages/homepage/HomePage.component";
 import ShopPage from './pages/shop/shop.component.jsx';
-
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
       </Switch>
-      
     </div>
   );
 }
 
 export default App;
+
+
