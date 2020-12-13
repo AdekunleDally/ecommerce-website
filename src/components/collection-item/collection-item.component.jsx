94d70c41e5e79352aab3310df56e31d2a88a303c
AdekunleDally <adekunledally@gmail.com>
2020-12-12 16:28:16 -0800

    cart-item component created, cart item reducer connected to the cart dropDown component

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -29,18 +29,3 @@ const mapDispatchToProps=dispatch=>({
 export default connect(null, mapDispatchToProps) (CollectionItem);
 
 
-
-// const array1=['a','b','c','x']
-// const array2=['z','y','i']
-
-// const arrFunc=(array1,array2)=>(
-//   for(let i=0; i<array1.length; i++){
-//     for(let j=0; j<array2.length; j++){
-//       if(array1[i]===array2[j]){
-//         return true
-//       }else{
-//         return false
-//       }
-//     }
-//   }
-// )
